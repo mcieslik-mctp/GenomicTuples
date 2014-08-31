0ce7d6145732a0afadca7c5bffd7217967992b20
Peter Hickey <peter.hickey@gmail.com>
2014-08-31 12:09:28 +1000

    Fix findOverlaps-based methods for GTuples and clarified findOverlaps and findOverlaps-based methods for GTuples/GTuplesList signatures.

--- R/findOverlaps-methods.R
+++ R/findOverlaps-methods.R
@@ -11,7 +11,7 @@
   # run findOverlaps with parameters chosen to select only equal matches. Then, 
   # intersect the Hits objects from each pair to create the final Hits object.
   # It's clunky, but it works.
-    
+  
   # Create all pairs and run findOverlaps
   size <- size(query)
   tuples_idx <- seq_len(size - 1L)
@@ -29,14 +29,14 @@
   s_tuples <- tuples(subject)
   
   pair_hits <- lapply(tuples_idx, function(i, q_seqnames, s_seqnames, q_strand, 
-                                   s_strand, q_tuples, s_tuples) {
+                                           s_strand, q_tuples, s_tuples) {
     query <- GRanges(q_seqnames, IRanges(q_tuples[, i], q_tuples[, i + 1]), 
                      q_strand)
     subject <- GRanges(s_seqnames, IRanges(s_tuples[, i], s_tuples[, i + 1]), 
                        s_strand)
     findOverlaps(query, subject, maxgap = 0L, minoverlap = 1L, type = "equal", 
                  select = select)
-    }, q_seqnames , s_seqnames, q_strand, s_strand, q_tuples, s_tuples)
+  }, q_seqnames , s_seqnames, q_strand, s_strand, q_tuples, s_tuples)
   
   # Intersect the Hits objects
   tuples_hits <- Reduce(intersect, pair_hits)
@@ -44,24 +44,22 @@
   return(tuples_hits)
 }
 
-
-# TODO: What happens if findOverlaps (type = 'equal') with m-tuples (m > 2) 
-# when positions are identical but strands differ, e.g. '+' vs. '+' (should be 
-# a match) or '+' vs. '-' (should not be a match).
-# TODO: Not quite sure why method breaks if 'ignore.strand = FALSE' rather than 
-# '...' is included in setMethod().
 # There is a specially defined method for findOverlaps when both the query and 
 # the subject are GTuples objects. This is to allow for "equal" matching 
 # between GTuples. All other methods are defined via inheritance.
-# If either the subject or the query is not a GTuples object then it defers to the findOverlaps method defined for GRanges objects. 
+# If either the subject or the query is not a GTuples object then it defers to 
+# the findOverlaps method defined for GRanges objects. 
 #' @export
 setMethod("findOverlaps", signature = c("GTuples", "GTuples"), 
           function(query, subject, maxgap = 0L, minoverlap = 1L, 
                    type = c("any", "start", "end", "within", "equal"), 
                    select = c("all", "first", "last", "arbitrary"), 
-                   ...) {
+                   ignore.strand = FALSE) {
             
             # Argument matching
+            if (!isSingleNumber(maxgap) || maxgap < 0L) {
+              stop("'maxgap' must be a non-negative integer")
+            }
             select <- match.arg(select)
             type <- match.arg(type)
             
@@ -96,9 +94,23 @@ setMethod("findOverlaps", signature = c("GTuples", "GTuples"),
               
               .findIdentical.GTuples(query, subject, select, ignore.strand)
             } else{
-              callNextMethod()
+              # TODO: Why doesn't callNextMethod() work?
+              #callNextMethod()
+              findOverlaps(query = as(query, "GRanges"), 
+                           subject = as(subject, "GRanges"), 
+                           maxgap = maxgap, 
+                           minoverlap = minoverlap,
+                           type = type,
+                           select = select,
+                           ignore.strand = ignore.strand)
             }
-          })
+          }
+)
+
+# findOverlaps,GTuplesList,GTuples-method; 
+# findOverlaps,GTuples,GTuplesList-method;
+# findOverlaps,GTuplesList,GTuplesList-method all inherit from the appropriate
+# GRanges/GRangesList methods.
 
 ### =========================================================================
 ### findOverlaps-based methods
@@ -114,8 +126,8 @@ setMethod("countOverlaps", signature = c("GTuples", "GTuples"),
                                              minoverlap = minoverlap, 
                                              type = match.arg(type), 
                                              ignore.strand = ignore.strand)) 
-            structure(tabulate(counts, NROW(query)), names=names(query))
-})
+            structure(tabulate(counts, NROW(query)), names = names(query))
+          })
 
 #' @export
 setMethod("overlapsAny", signature = c("GTuples", "GTuples"), 
@@ -142,3 +154,7 @@ setMethod("subsetByOverlaps", signature = c("GTuples", "GTuples"),
                                       select = "first",
                                       ignore.strand = ignore.strand))]
           })
+
+# findOverlaps-based methods with 
+# GTuples/GTuplesList signatures all inherit from the 
+# appropriate GRanges/GRangesList method
